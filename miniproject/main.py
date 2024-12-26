a1e90c21875821c649deef8f137a785fb8ede557
GamesByAGuy <joenovitt@gmail.com>
2024-12-26 21:24:50 +0400

    code for a simple window

--- miniproject/main.py
+++ miniproject/main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import pygame
+pygame.init()
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('game engine')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
